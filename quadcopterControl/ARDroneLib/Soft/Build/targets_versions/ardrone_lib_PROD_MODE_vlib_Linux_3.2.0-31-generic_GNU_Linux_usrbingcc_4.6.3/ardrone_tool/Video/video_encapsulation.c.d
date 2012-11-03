3b383a1f1bf9b0c17b5b55685f5b30855779ef3b
sytang <sarahyifang@gmail.com>
2012-11-03 15:09:47 -0400

    adding control package

---
+++ quadcopterControl/ARDroneLib/Soft/Build/targets_versions/ardrone_lib_PROD_MODE_vlib_Linux_3.2.0-31-generic_GNU_Linux_usrbingcc_4.6.3/ardrone_tool/Video/video_encapsulation.c.d
@@ -0,0 +1,23 @@
+video_encapsulation.o ../../Soft/Build/targets_versions/ardrone_lib_PROD_MODE_vlib_Linux_3.2.0-31-generic_GNU_Linux_usrbingcc_4.6.3/ardrone_tool/Video/video_encapsulation.c.d: \
+ ../..//Soft/Lib/ardrone_tool/Video/video_encapsulation.c \
+ /usr/include/stdio.h /usr/include/features.h \
+ /usr/include/i386-linux-gnu/bits/predefs.h \
+ /usr/include/i386-linux-gnu/sys/cdefs.h \
+ /usr/include/i386-linux-gnu/bits/wordsize.h \
+ /usr/include/i386-linux-gnu/gnu/stubs.h \
+ /usr/include/i386-linux-gnu/gnu/stubs-32.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stddef.h \
+ /usr/include/i386-linux-gnu/bits/types.h \
+ /usr/include/i386-linux-gnu/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stdarg.h \
+ /usr/include/i386-linux-gnu/bits/stdio_lim.h \
+ /usr/include/i386-linux-gnu/bits/sys_errlist.h \
+ /usr/include/i386-linux-gnu/bits/stdio.h \
+ /usr/include/i386-linux-gnu/bits/stdio2.h ../VP_Os/vp_os_types.h \
+ ../VP_Os/vp_os.h ../VP_Os/vp_os_rtmon.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stdint.h /usr/include/stdint.h \
+ /usr/include/i386-linux-gnu/bits/wchar.h ..//VP_Os/linux/vp_os_serial.h \
+ /usr/include/termios.h /usr/include/i386-linux-gnu/bits/termios.h \
+ /usr/include/i386-linux-gnu/sys/ttydefaults.h ../VP_Os/linux/intrin.h \
+ ../..//Soft/Common/video_encapsulation.h
---
+++ quadcopterControl/ARDroneLib/Soft/Build/targets_versions/ardrone_lib_PROD_MODE_vlib_Linux_3.2.0-31-generic_GNU_Linux_usrbingcc_4.6.3/ardrone_tool/Video/video_stage_latency_estimation.c.d
@@ -0,0 +1,94 @@
+video_stage_latency_estimation.o ../../Soft/Build/targets_versions/ardrone_lib_PROD_MODE_vlib_Linux_3.2.0-31-generic_GNU_Linux_usrbingcc_4.6.3/ardrone_tool/Video/video_stage_latency_estimation.c.d: \
+ ../..//Soft/Lib/ardrone_tool/Video/video_stage_latency_estimation.c \
+ ../..//Soft/Lib/ardrone_tool/Video/video_stage_latency_estimation.h \
+ ../..//Soft/Lib/ardrone_tool/Video/vlib_stage_decode.h \
+ ../VP_Api/vp_api.h ../VP_Os/vp_os_signal.h ../VP_Os/vp_os_types.h \
+ ../VP_Os/vp_os.h ../VP_Os/vp_os_rtmon.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stdint.h /usr/include/stdint.h \
+ /usr/include/features.h /usr/include/i386-linux-gnu/bits/predefs.h \
+ /usr/include/i386-linux-gnu/sys/cdefs.h \
+ /usr/include/i386-linux-gnu/bits/wordsize.h \
+ /usr/include/i386-linux-gnu/gnu/stubs.h \
+ /usr/include/i386-linux-gnu/gnu/stubs-32.h \
+ /usr/include/i386-linux-gnu/bits/wchar.h ..//VP_Os/linux/vp_os_serial.h \
+ /usr/include/termios.h /usr/include/i386-linux-gnu/bits/types.h \
+ /usr/include/i386-linux-gnu/bits/typesizes.h \
+ /usr/include/i386-linux-gnu/bits/termios.h \
+ /usr/include/i386-linux-gnu/sys/ttydefaults.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stddef.h ../VP_Os/linux/intrin.h \
+ ..//VP_Os/linux/vp_os_signal_dep.h /usr/include/pthread.h \
+ /usr/include/endian.h /usr/include/i386-linux-gnu/bits/endian.h \
+ /usr/include/i386-linux-gnu/bits/byteswap.h /usr/include/sched.h \
+ /usr/include/time.h /usr/include/i386-linux-gnu/bits/sched.h \
+ /usr/include/i386-linux-gnu/bits/time.h \
+ /usr/include/i386-linux-gnu/bits/timex.h /usr/include/xlocale.h \
+ /usr/include/i386-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/i386-linux-gnu/bits/setjmp.h ../VP_Api/vp_api_config.h \
+ ../VP_Api/vp_api_stage.h ../VP_Api/vp_api_supervisor.h \
+ ../../VLIB/video_codec.h ../../VLIB/video_controller.h \
+ ../VP_Api/vp_api_picture.h ../../FFMPEG/Includes/libavutil/avutil.h \
+ ../../FFMPEG/Includes/libavutil/common.h /usr/include/ctype.h \
+ /usr/include/errno.h /usr/include/i386-linux-gnu/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/i386-linux-gnu/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/inttypes.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include-fixed/limits.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/i386-linux-gnu/bits/posix1_lim.h \
+ /usr/include/i386-linux-gnu/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/i386-linux-gnu/bits/posix2_lim.h \
+ /usr/include/i386-linux-gnu/bits/xopen_lim.h \
+ /usr/include/i386-linux-gnu/bits/stdio_lim.h /usr/include/math.h \
+ /usr/include/i386-linux-gnu/bits/huge_val.h \
+ /usr/include/i386-linux-gnu/bits/huge_valf.h \
+ /usr/include/i386-linux-gnu/bits/huge_vall.h \
+ /usr/include/i386-linux-gnu/bits/inf.h \
+ /usr/include/i386-linux-gnu/bits/nan.h \
+ /usr/include/i386-linux-gnu/bits/mathdef.h \
+ /usr/include/i386-linux-gnu/bits/mathcalls.h \
+ /usr/include/i386-linux-gnu/bits/mathinline.h /usr/include/stdio.h \
+ /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stdarg.h \
+ /usr/include/i386-linux-gnu/bits/sys_errlist.h \
+ /usr/include/i386-linux-gnu/bits/stdio.h \
+ /usr/include/i386-linux-gnu/bits/stdio2.h /usr/include/stdlib.h \
+ /usr/include/i386-linux-gnu/bits/waitflags.h \
+ /usr/include/i386-linux-gnu/bits/waitstatus.h \
+ /usr/include/i386-linux-gnu/sys/types.h \
+ /usr/include/i386-linux-gnu/sys/select.h \
+ /usr/include/i386-linux-gnu/bits/select.h \
+ /usr/include/i386-linux-gnu/bits/sigset.h \
+ /usr/include/i386-linux-gnu/bits/select2.h \
+ /usr/include/i386-linux-gnu/sys/sysmacros.h /usr/include/alloca.h \
+ /usr/include/i386-linux-gnu/bits/stdlib.h /usr/include/string.h \
+ /usr/include/i386-linux-gnu/bits/string.h \
+ /usr/include/i386-linux-gnu/bits/string2.h \
+ /usr/include/i386-linux-gnu/bits/string3.h \
+ ../../FFMPEG/Includes/libavutil/attributes.h \
+ ../../FFMPEG/Includes/libavutil/avconfig.h \
+ ../../FFMPEG/Includes/libavutil/mem.h \
+ ../../FFMPEG/Includes/libavutil/avutil.h \
+ ../../FFMPEG/Includes/libavutil/error.h \
+ ../../FFMPEG/Includes/libavutil/mathematics.h \
+ ../../FFMPEG/Includes/libavutil/rational.h \
+ ../../FFMPEG/Includes/libavutil/intfloat_readwrite.h \
+ ../../FFMPEG/Includes/libavutil/log.h \
+ ../../FFMPEG/Includes/libavutil/pixfmt.h ../../VLIB/video_picture.h \
+ ../../VLIB/video_picture_defines.h ../../VLIB/video_gob.h \
+ ../../VLIB/video_macroblock.h ../../VLIB/P264/p264_common.h \
+ ../..//Soft/Lib/ardrone_tool/Video/video_stage_decoder.h \
+ ../..//Soft/Lib/ardrone_tool/Video/video_stage_ffmpeg_decoder.h \
+ ../../FFMPEG/Includes/libavcodec/avcodec.h \
+ ../../FFMPEG/Includes/libavutil/samplefmt.h \
+ ../../FFMPEG/Includes/libavutil/cpu.h \
+ ../../FFMPEG/Includes/libavcodec/version.h \
+ ../../FFMPEG/Includes/libavutil/audioconvert.h \
+ ../../FFMPEG/Includes/libavformat/avformat.h \
+ ../../FFMPEG/Includes/libavutil/dict.h \
+ ../../FFMPEG/Includes/libavformat/avio.h \
+ ../../FFMPEG/Includes/libavutil/common.h \
+ ../../FFMPEG/Includes/libavutil/log.h \
+ ../../FFMPEG/Includes/libavformat/version.h \
+ ../../FFMPEG/Includes/libswscale/swscale.h \
+ ../../VLIB/Stages/vlib_stage_decode.h ../VP_Os/vp_os_malloc.h \
+ /usr/include/assert.h /usr/include/i386-linux-gnu/sys/time.h

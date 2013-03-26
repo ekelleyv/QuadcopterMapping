1f5a39e386b1f1bbb35758cc9d7396d538d21c47
Ed Kelley <ekelleyv@gmail.com>
2013-03-26 15:54:38 -0400

    Tested waypoints

--- quadcopterCode/src/drone_controller.py
+++ quadcopterCode/src/drone_controller.py
@@ -4,6 +4,7 @@
 
 import roslib; roslib.load_manifest('quadcopterCode')
 import rospy
+import tf
 
 # Import the messages we're interested in sending and receiving
 from geometry_msgs.msg import Twist  	 # for sending commands to the drone
@@ -15,9 +16,20 @@ from drone_status import DroneStatus
 from basic_commands import BasicCommands
 from waypoints import waypoints
 
+from math import *
+
 CONTROLLER_PERIOD = 100 #ms
 
-LINEAR_
+LINEAR_ERROR = 200 #mm
+
+ANGULAR_ERROR = (2*PI)/16.0 #ASSUMING RADIANS
+
+LINEAR_MAX = .3 #Max tilt amount (unitless)
+
+ANGULAR_MAX = .2 #Max turn amount (unitless)
+
+LINEAR_GAIN = .1 # Pick good values
+ANGULAR_GAIN = .1 #
 
 
 class DroneController(BasicCommands):
@@ -27,19 +39,42 @@ class DroneController(BasicCommands):
 		self.z_est = 0
 		self.theta_est = 0
 
-		self.waypoints = waypoints
+		self.waypoints = waypoints("/home/ekelley/ros_workspace/sandbox/QuadcopterMapping/quadcopterCode/data/waypoints.txt")
+
+		self.current_waypoint = self.waypoints.get_waypoint();
 
 		#This is probably not how this is actually done
 		self.pos_sub = rospy.Subscriber('/localize/tf', tf, update_position)
 
 		self.controller_timer = rospy.Timer(rospy.Duration(COMMAND_PERIOD/1000.0),self.update_command)
 
+	def get_distance(self):
+		return sqrt((self.current_waypoint.x - self.x_est)**2 + (self.current_waypoint.z - self.z_est)**2 + (self.current_waypoint.z - self.z_est)**2)
+
+	def get_angle_diff(self):
+		return 
+
 	def update_position(self, position):
 		#some magic happens and I update the position values in the controller
 
-	
+
+
 	def update_command(self):
-		current_waypoint = self.waypoints[self.waypoint_index]
+		distance = self.get_distance()
+		angle = self.get_angle_diff()
+
+		#If it hit the target, move on
+		if ((distance < LINEAR_ERROR) and (angle < ANGLE_ERROR)):
+			self.current_waypoint = self.waypoints.get_waypoint()
+
+		x_diff = (self.current_waypoint.x - self.x_est)
+		y_diff = (self.current_waypoint.y - self.y_est)
+		z_diff = (self.current_waypoint.z - self.z_est)
+
+		#Define tilt as being within - and + MAX values
+
+		#SetCommand
+
 
 
 
--- quadcopterCode/src/waypoints.py
+++ quadcopterCode/src/waypoints.py
@@ -2,36 +2,37 @@
 
 class waypoints:
 	def __init__(self, filename):
-		self.waypoints = []
+		self.waypoint_list = []
 		self.new_waypoints = 0
-		self.waypoint_index = 0
-		read_file(filename)
+		self.waypoint_index= 0
+		self.read_file(filename)
 
 	def get_waypoint(self):
-		last_index = self.waypoints_index
-		self.waypoints_index += 1
-		return self.waypoints[]
+		last_index = self.waypoint_index
+		self.waypoint_index += 1
+		return self.waypoint_list[self.waypoint_index-1]
 
 	def add_waypoint(self, x, y, z, theta):
 		label = "new" + str(self.new_waypoints)
 		self.new_waypoints += 1
-		if (self.new_waypoints >= len(self.waypoints)):
-			return -1
-		else:
-			waypoints.append(waypoint(label, x, y, z, theta))
+		self.waypoint_list.append(waypoint(label, x, y, z, theta))
 
 	def read_file(self, filename):
 		#Read documentation for reading files
-		label = ""
-		x = 0
-		y = 0
-		z = 0
-		theta = 0
+		f = open(filename)
 
-		waypoints.append(waypoint(label, x, y, z, theta))
+		for line in f:
+			vals = line.split()
+			label = vals[0]
+			x = int(vals[1])
+			y = int(vals[2])
+			z = int(vals[3])
+			theta = int(vals[4])
+
+			self.waypoint_list.append(waypoint(label, x, y, z, theta))
 
 	def print_waypoints(self):
-		for waypoint in self.waypoints:
+		for waypoint in self.waypoint_list:
 			waypoint.print_waypoint()
 
 class waypoint:
@@ -43,5 +44,25 @@ class waypoint:
 		self.theta = theta
 
 	def print_waypoint(self):
-		print "%s : (%2f, %2f, %2f, %2f)\n" % (self.x, self.y, self.z, self.theta)
+		print "%s : (%f, %f, %f, %f)" % (self.label, self.x, self.y, self.z, self.theta)
 
+
+def main():
+	test = waypoints("/home/ekelley/ros_workspace/sandbox/QuadcopterMapping/quadcopterCode/data/waypoints.txt")
+	print "File Waypoints"
+	test.print_waypoints()
+
+	test.add_waypoint(5, 5, 5, 5)
+	print "Add Waypoints"
+	test.print_waypoints()
+
+	print "Getting 0"
+	waypoint = test.get_waypoint()
+	waypoint.print_waypoint()
+
+	print "Getting 1"
+	waypoint = test.get_waypoint()
+	waypoint.print_waypoint()
+
+if __name__ == '__main__':
+	main()
